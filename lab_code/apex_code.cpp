8610c9befa1ab1968c5eb38f30d74eaccba31725
Alex Chow <45185445+alex0091845@users.noreply.github.com>
2024-06-23 11:00:10 +0800

    Update apex_code.cpp

--- lab_code/apex_code.cpp
+++ lab_code/apex_code.cpp
@@ -7,39 +7,55 @@ using namespace std;
 
 void email_concat(stringstream& cin, stringstream& cout)
 {
-    // Place code for getting the street address
+    // Place code for concatenating emails together with colons.
+    // Stop reading in inputs when you see the string "END".
     // Pretend this is your main function and you can use cin/cout
     // as you would in main.
 
     // If you want to print to the console for debugging use std::cout.
     // Use cout for your final solution.
+
+    // Remove these comments and write your own explaining your code
+    // once done.
 }
 
 void donation_total(stringstream &cin, stringstream &cout) {
-    // Place code for outputting the total sales of a coffee item
+    // Place code for outputting the total donations and if applicable,
+    // output "Send a t-shirt" with an endl.
     // Pretend this is your main function and you can use cin/cout
     // as you would in main.
 
     // If you want to print to the console for debugging use std::cout.
     // Use cout for your final solution.
+
+    // Remove these comments and write your own explaining your code
+    // once done.
 }
 
 void average_books_checked_out(std::stringstream &cin, std::stringstream &cout)
 {
-    // Place code for reading the first name and country here
+    // Place code for printing out the average books checked out per month.
+    // If applicable, print out "Extend hours!!".
     // Pretend this is your main function and you can use cin/cout
     // as you would in main.
 
     // If you want to print to the console for debugging use std::cout.
     // Use cout for your final solution.
+
+    // Remove these comments and write your own explaining your code
+    // once done.
 }
 
 void rectangles(std::stringstream &cin, std::stringstream &cout)
 {
-    // Place code for reading the first name and country here
+    // Place code for printing out a rectangle with dimensions and 
+    // character specified from cin.
     // Pretend this is your main function and you can use cin/cout
     // as you would in main.
 
     // If you want to print to the console for debugging use std::cout.
     // Use cout for your final solution.
+
+    // Remove these comments and write your own explaining your code
+    // once done.
 }
