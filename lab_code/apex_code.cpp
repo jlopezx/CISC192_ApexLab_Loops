ebf0686f2a2ec67120a2b7a69065e93d4e2401db
Josh <joshualopezx@gmail.com>
2025-07-01 22:54:21 -0700

    Loops_lab_update_2

--- lab_code/apex_code.cpp
+++ lab_code/apex_code.cpp
@@ -28,6 +28,9 @@
 
 using namespace std;
 
+const int STARTING_VALUE = 0; // Using this const to avoid using magic numbers. 0 is where my for-loop
+								  // variables are going to start.
+
 void email_concat(stringstream& cin, stringstream& cout)
 {
 	string userInput;			// Used to hold our user's input
@@ -42,16 +45,19 @@ void email_concat(stringstream& cin, stringstream& cout)
 	// good practice.
 	if (userInput.find("END") == std::string::npos) {
 		cout << "Need to provide END in your input" << endl;
+
 	} else {
 		// In in this branch, we have an "END" in our string. Here we will find the END's index and store it
 		// to isEnd which whill hold our index to knwo if we reached the end.
 		isEnd = userInput.find("END");
 		// Easily could of been a different loop, but since we know how many times we need to loop a FOR
 		// loop is the most appropriate.
-		for (int i = 0; i < isEnd; ++i) {
+		for (int i = STARTING_VALUE; i < isEnd; ++i) {
 			// If looks for spaces to replace them with colons. We use the replace function to replace the
 			// space. This function is given to us by the developers of C++ under the <string> file.
 			if (userInput[i] == ' ') {
+				// .replace() method takes in the index of where to start replacement, the number of
+				// characters to replace, and the string to replace it with.
 				userInput.replace(i, 1, ":");
 			}
 			// Inside the foor loop, we append each character to the finalOutput string to build our final
@@ -64,8 +70,12 @@ void email_concat(stringstream& cin, stringstream& cout)
 }
 
 void donation_total(stringstream &cin, stringstream &cout) {
-	double donated = 0;			// Holds the current donated amount. Initialized to ensure it's 0.
-	double totalDonated = 0;	// Adds the total donated amount. Initialized to ensure its truly 0.
+	const int T_SHIRT_PROMO = 100; // THe amount needed to send a t-shirt to the donators.
+	const int MININUM_VALUE = 0;   // Used to hold the minimum value before we go into negative numbers
+								   // which in this case is 0.
+
+	double donated = 0;			   // Holds the current donated amount. Initialized to ensure it's 0.
+	double totalDonated = 0;	   // Adds the total donated amount. Initialized to ensure its truly 0.
 
 	// We do not know how many donations are made so we use a do-while loop and check if the donated amount
 	// is not negative. This loop ensures we at least run the loop code one time.
@@ -75,19 +85,23 @@ void donation_total(stringstream &cin, stringstream &cout) {
 		totalDonated += donated;
 		cin >> donated;
 
-	} while (donated >= 0);
+	} while (donated >= MININUM_VALUE);
 
 	// If donations are more than $100, we send a t-shirt, twice.
-	if (totalDonated > 100) {
+	if (totalDonated > T_SHIRT_PROMO) {
 		cout << "Send a t-shirt" << endl << "Send a t-shirt" << endl;
 	}
 	// Print total donations with fixed to neverr display scientific notation, and precision of 2 for x.xx
-	// format
+	// format.
 	cout << "Total donations made $" << fixed << setprecision(2) << totalDonated;
 }
 
 void average_books_checked_out(std::stringstream &cin, std::stringstream &cout)
 {
+	const int monthsInYear = 12;
+	const int monthlyThreshold = 1268;
+	const int averageMonthlyLimit = 2500;
+
 	int booksChecked; 			// Hold the number of books checked out by each user.
 	int totalBooksChecked = 0; 	// Holds the total number of books checked out.
 	int monthlyBooksAverage;	// This isn't needed but provided better code readability. Used to hold
@@ -99,7 +113,7 @@ void average_books_checked_out(std::stringstream &cin, std::stringstream &cout)
 	// A for loop would have been more appropriate, but I decided to use a while to keep in the spirit
 	// of using different loop structures. This loop will take 12 inputs from the user and add them
 	// together.
-	while(count < 12) {
+	while(count < monthsInYear) {
 		// Puts the monthly checked out books into booksChecks. It will read these month-by-month for the
 		// entire year.
 		cin >> booksChecked;
@@ -109,7 +123,7 @@ void average_books_checked_out(std::stringstream &cin, std::stringstream &cout)
 
 		// Checks if inputs are over 1268, if any are over 1268, then overThreshold will be false regardless
 		// if the rest of the inputs are over 1268.
-		overThreshold = overThreshold && (booksChecked > 1268);
+		overThreshold = overThreshold && (booksChecked > monthlyThreshold);
 
 		// Pre-increments count. Used over post-increment as its more efficient. (Avoids using the temporary
 		// copy).
@@ -117,10 +131,10 @@ void average_books_checked_out(std::stringstream &cin, std::stringstream &cout)
 	}
 
 	// Caculates average of books checked out each month and stores the result to booksAverage.
-	monthlyBooksAverage = totalBooksChecked / 12;
+	monthlyBooksAverage = totalBooksChecked / monthsInYear;
 
 	cout << "Average monthly circulation: " << monthlyBooksAverage << " books" << endl;
-	if (overThreshold && monthlyBooksAverage > 2500) {
+	if (overThreshold && monthlyBooksAverage > averageMonthlyLimit) {
 		cout << "Extend hours!!";
 	}
 
@@ -142,8 +156,8 @@ void rectangles(std::stringstream &cin, std::stringstream &cout)
     // Nested for loops. Using typical i j variables for the loop bodies. Outer loops is rows and inner
     // loop is columns because of the direction things are printed. Columns are printed first, then
     // we move to the next row with an endl.
-    for (int i= 0; i < userRows; ++i) {
-    	for (int j = 0; j < userColumns; ++j) {
+    for (int i = STARTING_VALUE; i < userRows; ++i) {
+    	for (int j = STARTING_VALUE; j < userColumns; ++j) {
     		cout << charInput << " ";
     	}
     	// This endl moves to the next row.
